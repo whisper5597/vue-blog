9e01343904f18fe1a77ace26f93c7ab508f73939
whisper5597 <1040233574@qq.com>
2025-07-25 17:40:18 +0800

    build: 添加 base 配置以支持 GitHub Pages 部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-blog/', // github pages 部署需要
   plugins: [
     vue(),
     // 移除tailwindcss插件，因为它会通过PostCSS配置加载
